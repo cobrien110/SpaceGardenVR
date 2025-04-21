1e2c6b11953f1c3043b4c27107879814ee5229d9
cobrien110 <89876727+cobrien110@users.noreply.github.com>
2025-04-21 12:42:43 -0400

    working shop and seed bug fix

---
+++ Assets/Scripts/ShopManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    [SerializeField] private Transform spawnLocation;
+    [SerializeField] private float dirRandomRange = 0.15f;
+    [SerializeField] private float launchForce = 0.5f;
+    private StatTracker ST;
+    public float fireRatePerUpgrade = 0.05f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ST = GameObject.FindAnyObjectByType<StatTracker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnAndFire(GameObject item)
+    {
+        Debug.Log("Spawning shop item: " + item.name);
+        GameObject drop = Instantiate(item, spawnLocation.position, spawnLocation.rotation);
+        Rigidbody RB = drop.GetComponent<Rigidbody>();
+        if (RB == null) return;
+
+        //get random force dir
+        float dirX = Random.Range(-dirRandomRange, dirRandomRange);
+        float dirY = Random.Range(-dirRandomRange, dirRandomRange);
+
+        // Start with forward direction
+        Vector3 baseDir = spawnLocation.forward;
+
+        // Add slight variation in right and up directions
+        Vector3 variedDir = baseDir + spawnLocation.right * dirX + spawnLocation.up * dirY;
+
+        // Normalize to maintain consistent force
+        Vector3 finalForce = variedDir.normalized * launchForce;
+
+        RB.AddForce(finalForce, ForceMode.Impulse); // optional: use Impulse for instant push
+    }
+
+    public void SpawnAndFire(GameObject item, float force, int cost)
+    {
+        Debug.Log("Spawning shop item: " + item.name);
+        GameObject drop = Instantiate(item, spawnLocation.position, spawnLocation.rotation);
+        Rigidbody RB = drop.GetComponent<Rigidbody>();
+        if (RB == null) return;
+
+        //get random force dir
+        float dirX = Random.Range(-dirRandomRange, dirRandomRange);
+        float dirY = Random.Range(-dirRandomRange, dirRandomRange);
+
+        // Start with forward direction
+        Vector3 baseDir = spawnLocation.forward;
+
+        // Add slight variation in right and up directions
+        Vector3 variedDir = baseDir + spawnLocation.right * dirX + spawnLocation.up * dirY;
+
+        // Normalize to maintain consistent force
+        Vector3 finalForce = variedDir.normalized * force;
+
+        RB.AddForce(finalForce, ForceMode.Impulse); // optional: use Impulse for instant push
+
+        ST.money -= cost;
+    }
+
+    public void Upgrade(int i, int cost)
+    {
+        if (i == 1)
+        {
+            ST.waterCanFireRate -= fireRatePerUpgrade;
+            if (ST.waterCanFireRate < 0.05f) ST.waterCanFireRate = 0.05f;
+        } else if (i == 2)
+        {
+            ST.waterCanDropAmount += 1;
+        }
+
+        ST.money -= cost;
+    }
+}
